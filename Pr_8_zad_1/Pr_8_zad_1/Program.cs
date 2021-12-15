9a4d832dded7cf03e2b494fd7317b6c1c9e974df
Morok0 <95218742+Morok0@users.noreply.github.com>
2021-12-15 10:37:58 +0300

    Add files via upload

---
+++ Pr_8_zad_1/Pr_8_zad_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Pr_8_zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            String line = Console.ReadLine();
+            Console.WriteLine("Исходная строка: " + line);
+            Console.WriteLine("Исходный текст: {0}\nВведите длину слов: ", line);
+            int length = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{string.Join(" ", Regex.Matches(line, @"\w+").Cast<Match>().Where(t => t.Length == length))}");
+
+        }
+    }
+}
